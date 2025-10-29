dce66a5b3d24538bf3f52e7e63ecee1f14491ba9
btin <b.tintuk@gmail.com>
2025-10-29 22:35:50 +0700

    20251029-223551 update-manga-item-not-yet-publish

--- pages/25_ioub___61k/scripts/mangaItemsList.js
+++ pages/25_ioub___61k/scripts/mangaItemsList.js
@@ -1,7 +1,7 @@
 import { CONSTANT } from '../constant.js';
 import { UTILS } from './utils.js';
 import { MANGA_CARTS } from './mangaCarts.js';
-const { formatText, getCart } = UTILS;
+const { formatText, formatPrice, getCart } = UTILS;
 
 const { MANGA_ITEMS } = CONSTANT;
 const { addMangaItemToCart } = MANGA_CARTS;
@@ -67,20 +67,33 @@ const renderMangaItemsList = (document) => {
           <div class="flex flex-col p-3 gap-2 text-black rounded-b-2xl bg-white/76 backdrop-blur-xl">
             <h2 class="text-lg font-bold">${formatText(name)}</h2>
             <ul class="flex items-center justify-between text-sm">
-              <li class="text-red-800 text-lg font-semibold">${price} VNĐ</li>
+              <li class="text-red-800 text-lg font-semibold">${formatPrice(price)} VNĐ</li>
               <li class="text-slate-500 text-xs font-extralight sold-count">Đã bán 01</li>
             </ul>
           </div>
         </div>
         <!-- ${isSaled ? `<div class="sale-badge" data-sale="${salePercent}"></div>` : ''} -->
-        <div
-          id="addMangaToCart"
-          data-id="${item.id}"
-          class="hover-buy-item absolute top-0 left-0 w-full h-full rounded-2xl border-white/20 bg-white/10 backdrop-blur-xl shadow-[0_8px_32px_rgba(31,38,135,0.1)] flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer">
-          <i
-            data-id="${item.id}"
-            class="fa-solid fa-plus text-white/60 text-8xl hover:text-white/80 transition-all duration-300 cursor-pointer"></i>
-        </div>
+        ${!isPublished 
+          ? `
+            <div
+                id="blockMangaToCart"
+                data-id="${item.id}"
+                class="hover-buy-item absolute top-0 left-0 w-full h-full rounded-2xl border-white/20 bg-white/10 backdrop-blur-xl shadow-[0_8px_32px_rgba(31,38,135,0.1)] flex flex-col items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
+                <i
+                  data-id="${item.id}"
+                  class="fa-solid fa-exclamation text-white/60 text-8xl hover:text-white/80 transition-all duration-300"></i>
+                  <span class="text-white/60 text-sm font-light">Sắp có hàng. Vui lòng chờ</span>
+              </div>`
+          : `
+              <div
+                id="addMangaToCart"
+                data-id="${item.id}"
+                class="hover-buy-item absolute top-0 left-0 w-full h-full rounded-2xl border-white/20 bg-white/10 backdrop-blur-xl shadow-[0_8px_32px_rgba(31,38,135,0.1)] flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer">
+                <i
+                  data-id="${item.id}"
+                  class="fa-solid fa-plus text-white/60 text-8xl hover:text-white/80 transition-all duration-300 cursor-pointer"></i>
+              </div>`
+        }
       </div>
     `
     mangaItems.appendChild(mangaItem);
