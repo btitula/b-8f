7f8f8d41090cff92f0d4ce56a2f7196542e31a7d
btin <b.tintuk@gmail.com>
2025-11-01 10:25:04 +0700

    20251101-102505 cleanup-ui-and-codebase

--- apps/tools/calendar/js/utils.js
+++ apps/tools/calendar/js/utils.js
@@ -1,27 +1,44 @@
-//#region Helper Functions
-const normalizeDate = (date) => {
-  return date.toISOString().split("T")[0]; // Get YYYY-MM-DD format
+const initElements = () => {
+  const calendar = document.querySelector(".calendar");
+  const currentMonthYear = document.getElementById("current-month-year");
+  const prevMonthBtn = document.getElementById("button-previous-month");
+  const nextMonthBtn = document.getElementById("button-next-month");
+
+  const dialog = document.getElementById("booking-dialog");
+  const openModalBtn = document.getElementById("button-open-modal-update-working-day");
+  const closeDialogBtn = document.getElementById("button-close-dialog");
+
+  return { calendar, currentMonthYear, prevMonthBtn, nextMonthBtn, dialog, openModalBtn, closeDialogBtn };
 }
 
-const clearSelection = () => {
-  document.querySelectorAll(".day").forEach((day) => {
-    day.classList.remove("afternoon", "morning", "full", "disabled");
-  });
+const getCurrenntDate = () => {
+  let today = new Date(
+    Date.UTC(
+      new Date().getUTCFullYear(),
+      new Date().getUTCMonth(),
+      new Date().getUTCDate()
+    )
+  );
+  let currentMonth = today.getUTCMonth();
+  let currentYear = today.getUTCFullYear();
+
+  return { currentMonth, currentYear };
 }
 
-const disableFullyOccupiedTiles = () => {
+const handleDayClick = (day) => {
+  const thisDay = document.querySelector(`[data-date="${day.dataset.date}"]`);
+  thisDay.classList.add("selected");
+
+  // check and remove .selected from another day
   document.querySelectorAll(".day").forEach((day) => {
-    if (
-      day.classList.contains("full") || (day.classList.contains("morning") && day.classList.contains("afternoon"))
-    ) {
-      day.classList.add("disabled");
+    if (day.dataset.date !== thisDay.dataset.date) {
+      day.classList.remove("selected");
     }
   });
 }
 
 export const UTILS = {
-  normalizeDate,
-  clearSelection,
-  disableFullyOccupiedTiles
+  getCurrenntDate,
+  initElements,
+  handleDayClick
 }
-//#endregion
