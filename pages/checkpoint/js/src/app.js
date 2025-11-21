514d5191b8e5a02b41108541e4f30f6a389efa6a
btin <b.tintuk@gmail.com>
2025-11-21 23:38:14 +0700

    20251121-233814 add-albums-quick-picks

--- pages/checkpoint/js/src/app.js
+++ pages/checkpoint/js/src/app.js
@@ -6,9 +6,11 @@ import Footer from "@/components/Footer";
 const app = async () => {
   return `
     ${await SideBar()}
-    <div class="fixed top-0 left-60 right-0 bottom-0">
-      ${await Header()}
-      <main id="js-body" class="p-4 h-full overflow-auto"></main>
+    <div class="fixed top-0 left-60 right-0 bottom-0 flex justify-center">
+      <div class="w-full relative">
+        ${await Header()}
+        <main id="js-body" class="p-4 h-full overflow-auto"></main>
+      </div>
     </div>
   `
 }
